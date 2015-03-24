582db8c28ee2649709c6f7f4bbbdc86dee74b988
robclu <robclu1818@gmail.com>
2015-03-24 23:15:31 +0200

    Added google tests (although not yet with the aspect compiler)

---
+++ Examples/VectorAdd/tests/tests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+//#include "VectorAdd.h"
+
+// Put all tests here
+
+int main(int argc, char **argv) 
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
