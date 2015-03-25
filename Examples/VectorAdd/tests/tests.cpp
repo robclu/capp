7c9915f812ca6422eed10e89bfca5e7b9cbffd94
robclu <robclu1818@gmail.com>
2015-03-25 09:30:44 +0200

    Weaver working, Gtest runs with aspects =D =D

--- Examples/VectorAdd/tests/tests.cpp
+++ Examples/VectorAdd/tests/tests.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 //#include "VectorAdd.h"
 
+//#include <boost/test/unit_test.hpp>
+
 // Put all tests here
 
-int main(int argc, char **argv) 
+int main(int argc, char** argv) 
 {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
