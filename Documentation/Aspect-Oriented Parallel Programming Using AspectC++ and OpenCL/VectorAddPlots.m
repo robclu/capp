70f60e91b6b6a2d02d98c8f9ffe7b68214d1031e
robclu <robclu1818@gmail.com>
2015-06-20 20:41:20 +0200

    Finished PDF =)

--- Documentation/Aspect-Oriented Parallel Programming Using AspectC++ and OpenCL/VectorAddPlots.m
+++ Documentation/Aspect-Oriented Parallel Programming Using AspectC++ and OpenCL/VectorAddPlots.m
@@ -11,15 +11,10 @@ natimes1 = importdata('vanaresults1.txt');
 natimes2 = importdata('vanaresults1.txt');
 natimes3 = importdata('vanaresults1.txt');
 
-% Load in sequential results
-stimes1 = importdata('vasresults1.txt');
-stimes2 = importdata('vasresults1.txt');
-stimes3 = importdata('vasresults1.txt');
-
 % Calculate the means of each dataset
 atimes_mean = get_mean( atimes1, atimes2, atimes3 );
 natimes_mean = get_mean( natimes1, natimes2, natimes3 );
-stimes_mean = get_mean( stimes1, stimes2, stimes3 );
+stimes_mean = calc_average( 90, 'vasresults', '.txt' );
 
 figure();
 hold all;
@@ -27,7 +22,7 @@ plot( stimes_mean(:,1) .* 10e-7, stimes_mean(:,2) .* 10e-9, '-bx');
 plot( atimes_mean(:,1) .* 10e-7, atimes_mean(:,2) .* 10e-9, '-rx');
 plot( natimes_mean(:,1) .* 10e-7, natimes_mean(:,2) .* 10e-9, '-gx');
 xlim([0 10]);
-%legend( 'CPU','CAPP', 'OpenCL', 'location', 'northwest');
+legend( 'CPU','CAPP', 'OpenCL', 'location', 'northwest');
 title('Graph Showing Run-time vs. Vector Size for the SAXPY Problem');
 ylabel('Time (seconds)');
 xlabel('Vector Size (Millions)')
